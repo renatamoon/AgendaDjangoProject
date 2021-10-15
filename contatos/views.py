298eca3911f77d0eba6ca25772c96ab81e3dfe03
renata monteiro <r.cardoso.monteiro1@gmail.com>
2021-10-15 11:20:31 -0300

    novos itens de aprendizado em Django

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from . models import Contato
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -17,18 +19,30 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
 
+#pega
 def busca(request):
     termo = request.GET.get('termo')
 
-    contatos = Contato.objects.all().filter(
-        nome__icontains=termo,
-        mostrar=True
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+                            #o campo Value simula um campo que existe na minha base de dados
+                            #filtrar por um termo - no caso Nome ou Sobrenome
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo) #coloco o iconts
+        #para também mostrar os campos do telefone
     )
-    paginator = Paginator(contatos, 6)
+    paginator = Paginator(contatos, 3)
 
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
